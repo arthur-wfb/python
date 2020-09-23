b83a75560b0eac1ad4ff4baab212f5ba23ebde4e
Arthur Shaidullin <ururu2909@gmail.com>
2020-09-23 08:19:59 +0400

    ДЗ 2

--- test/test_calc.py
+++ test/test_calc.py
@@ -11,13 +11,22 @@ def test_calcline(a, b, answer):
     assert line(a, b) == answer
 
 
-@pytest.mark.parametrize('a, b, c, answer', [(1, -3, -4, [4, -1])])
-def test_calcSquare(a, b, c, answer):
+@pytest.mark.parametrize('a, b, c, answer', [
+    (1, 2, 1, [-1]),
+    (2, 1, 3, [4]),
+    (1, 'Masha', 5, [])
+])
+def test_square(a, b, c, answer):
     assert square(a, b, c) == answer
 
 
-@pytest.mark.parametrize('a, b, c, d, answer', [(1, -5, 8, -4, [1.0, 2.0, 2.0])])
-def test_calcCube(a, b, c, d, answer):
+@pytest.mark.parametrize('a, b, c, d, answer', [
+    (1, 1, 1, 1, [-0.9999999999999998, (-1.1102230246251565e-16+1j), (-1.1102230246251565e-16-1j)]),
+    (2, 4, 5, 1, []),
+    (3, 1, 9, 12, [12]),
+    (1, 'Masha', 5, 2, [])
+])
+def test_cube(a, b, c, d, answer):
     assert cube(a, b, c, d) == answer
 
 
